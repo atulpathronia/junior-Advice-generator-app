70e82d8e9f3ce74263b0299fd4664c2a0b7b1b9d
atulpathronia <atulpathronia@gmail.com>
2022-06-05 18:41:53 +0530

    finished

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const advice = document.querySelector("h2");
+const adviceNum = document.querySelector(".adviceNum");
+const button = document.querySelector(".btn");
+console.log(advice);
+console.log(adviceNum);
+
+const fetchAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  console.log(data);
+  adviceNum.innerHTML = `ADVICE # ${data.slip.id}`;
+  advice.innerHTML = `${data.slip.advice}`;
+};
+fetchAdvice();
+
+button.addEventListener("click", fetchAdvice);
